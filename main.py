d53dcc2f9cd9706f7c873a41e66e10164950dc76
Serban David <dudadud4418@gmail.com>
2025-10-29 16:20:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import fastapi, uvicorn
+
+from routes.health import health_router
+
+app = fastapi.FastAPI()
+
+app.include_router(health_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
